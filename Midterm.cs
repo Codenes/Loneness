4db0902707f9b31681a194e751e6266aaabe4232
Codenes <122738487+Codenes@users.noreply.github.com>
2023-03-13 17:05:01 +0700

    Add files via upload

---
+++ Midterm.cs
@@ -0,0 +1,15 @@
+using System;
+class program{
+static void Main(string[] args){
+    
+}
+    Console.WriteLine("int Day of Week :");
+    int Day = int.Parse(Console.ReadLine());
+    if( Day == 8 Day == 9 Day == 10 Day == 11 Day == 12 ||Day == 13);{
+
+        Console.Write("please number 1-7");
+     
+}
+
+
+
